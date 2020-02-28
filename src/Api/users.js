28ea996feabc432e988aeff62331849ef8a0aaf2
Yuliy Yefymenko <uliy86@gmail.com>
2020-02-28 18:01:29 +0100

    add simple response, getAll and getOneByID

--- src/Api/users.js
+++ src/Api/users.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Users = require("../Models/User");
 
-router.get("/", (req, res) => {
-    res.send("all users")
+router.get("/", async (req, res) => {
+    const users = await Users.find({});
+    let result = users.length?users:"cant find any users"
+    res.send(result);
 })
 
-router.get("/:userId", (req, res) => {
-    res.send("get user by id ",req.params.userId);
+router.get("/:userId",async (req, res) => {
+    const users = await Users.findOne({
+      _id: new mongoose.Types.ObjectId(req.params.userId)
+    });    
+    console.log(users);
+    let result = users ? users : "can't find user";
+    res.send(result);
 });
 
 module.exports = router;
