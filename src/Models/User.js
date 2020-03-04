5cc227d9d0207dd288dedf9858b9db243fafc7f1
Yuliy Yefymenko <uliy86@gmail.com>
2020-03-04 12:26:13 +0100

    add requered to User model

--- src/Models/User.js
+++ src/Models/User.js
@@ -7,10 +7,22 @@ const mongoose = require('mongoose');
 - Event date (required, simple date picker)
 */
 var userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  eventDate: [Date]
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  eventDate: {
+    type: Date,
+    required: true
+  }
 });
 
 var User = mongoose.model("User", userSchema);
