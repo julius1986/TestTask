04cf03af882ed509f7b10a1e7fbc3ed9861bd365
Yuliy Yefymenko <uliy86@gmail.com>
2020-03-06 18:58:57 +0100

    refactor test, add test get user by Id

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,32 +1,51 @@
 const request = require("supertest");
-const {app,server} = require("../app");
+const {
+  app,
+  server
+} = require("../app");
 const mongoose = require('mongoose');
+const User = require('../Models/User');
 
-beforeAll(function() {
-  console.log("Before");
-});
-
-afterAll(function() {
+afterAll(function () {
   mongoose.disconnect()
   server.close();
 });
 
-
 describe("Get request '/users'", () => {
-  it("should get all users",  (done) => {
-     request(app)
+  it("should get all users", done => {
+    request(app)
       .get("/users")
       .expect("Content-Type", /json/)
       .expect(200)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         done()
+      });
+    })
+
+  it("should get one user by id", async (done)=>{
+    const testModel = {
+      firstName: "Vasya",
+      lastName: "Pushkin",
+      email: "misha@gmail.com",
+      eventDate: 1583516437
+    };
+    const usr = await (new User(testModel)).save();
+    const input = usr._id;
+    request(app)
+      .get("/users/"+input)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end(async function(err, res) {
+        await User.deleteOne({_id:usr._id})
+        if (err) return done(err);       
+        expect(res.body).toMatchObject(usr);
+        done();
       });
   });
 });
 
 describe("Post request '/users'", () => {
-  
   it("should add new user, and back 'true' ", done => {
     const input = {
       firstName: "Misha",
@@ -34,32 +53,40 @@ describe("Post request '/users'", () => {
       email: "misha@gmail.com",
       eventDate: "2020-03-04T10:56:20.833Z"
     };
-    const output = { result: true, body:input };
+    const output = {
+      result: true,
+      body: input
+    };
     request(app)
       .post("/users")
       .send(input)
       .set("Accept", "application/json")
       .expect(200)
-      .end(function(err, res) {
-         expect(res.body).toMatchObject(output);
+      .end(async function (err, res) {
+        await User.deleteOne({_id:res.body.body._id})
         if (err) return done(err);
+        expect(res.body).toMatchObject(output);        
         done();
       });
   });
 
-  it("should try to add new user(with wrong email) and back 'false' ", done => {
-    const output = { result: false, body: "bad email" };
+  it("should try to add new user(with wrong email) and back 'false' with message ", done => {
+    const output = {
+      result: false,
+      body: "bad email"
+    };
+    const input = {
+      firstName: "Misha",
+      lastName: "Petrow",
+      email: "@gmail.com",
+      eventDate: "1583319380833"
+    };
     request(app)
       .post("/users")
-      .send({
-        firstName: "Misha",
-        lastName: "Petrow",
-        email: "@gmail.com",
-        eventDate: "1583319380833"
-      })
+      .send(input)
       .set("Accept", "application/json")
       .expect(200, output)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         done();
       });
